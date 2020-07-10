2ba0997fe9fa7e8480a193b8a81c96f20ab7ac25
SaiKrishna2681 <45065762+SaiKrishna2681@users.noreply.github.com>
2020-07-10 10:37:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,13 @@
-resource "null_resource" "sample1" {}
+provider "aws" {
+  region = "us-east-2"
+  access_key="AKIA5CAEYHEBV6VROLJQ"
+  secret_key="+W+cOLbDsQtps/pEfjLQFHBJGcG55WcRuYaISD2y"
+}
+
+resource "aws_instance" "wb" {
+   ami  = "ami-026dea5602e368e96"
+   instance_type = "t2.micro"
+  tags = {
+    Name = "webserver"
+  }
+}
