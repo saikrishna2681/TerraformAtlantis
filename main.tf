3fa5eed36e9f49b175657ff0994532cc26500230
SaiKrishna2681 <45065762+SaiKrishna2681@users.noreply.github.com>
2020-07-11 18:36:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "web2" {
    ami  = "ami-026dea5602e368e96"
    instance_type = "t2.micro"
+   iam_instance_profile = "${}"
   tags = {
     Name = "webserver2"
   }
