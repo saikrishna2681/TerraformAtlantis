5d144d3b3be6175cb9348f9f8b9939f56583495d
SaiKrishna2681 <45065762+SaiKrishna2681@users.noreply.github.com>
2020-07-13 14:38:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   region = "us-east-2"
+  access_key="AKIA5CAEYHEB7RYQ4LL4"
+  secret_key="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"
 }
 
-resource "aws_instance" "web2" {
+resource "aws_instance" "web" {
    ami  = "ami-026dea5602e368e96"
    instance_type = "t2.micro"
-   iam_instance_profile = "${aws_iam_instance_profile.test_profile.name}"
   tags = {
-    Name = "webserver3"
+    Name = "webserver1"
   }
 }
-
